f13871865496e0422b3f084bc4c739c09b739fab
Aris Ripandi <riipandi@gmail.com>
2022-08-04 12:40:06 +0700

    add Plausible analytics

---
+++ app/constants/analytics.ts
@@ -0,0 +1,4 @@
+export const plausibleConfig = {
+  apiHost: 'https://stats.web.id',
+  domain: 'prismix.fly.dev',
+}
--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react'
 import { RemixBrowser } from '@remix-run/react'
 import { hydrateRoot } from 'react-dom/client'
+import Plausible from 'plausible-tracker'
+
+import { plausibleConfig } from '~/constants/analytics'
 
 function hydrate() {
+  const { trackPageview } = Plausible(plausibleConfig)
+
   React.startTransition(() => {
     hydrateRoot(
       document,
@@ -11,6 +16,9 @@ function hydrate() {
       </React.StrictMode>,
     )
   })
+
+  Plausible(plausibleConfig)
+  trackPageview()
 }
 
 if (window.requestIdleCallback) {
