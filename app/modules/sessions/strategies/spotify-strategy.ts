ebc874bc0456f94d65dea50449315e5b80fbd314
Aris Ripandi <riipandi@gmail.com>
2022-09-10 13:17:33 +0700

    fixed Spotify authentication

--- app/modules/sessions/strategies/spotify-strategy.ts
+++ app/modules/sessions/strategies/spotify-strategy.ts
@@ -1,16 +1,20 @@
 import invariant from 'tiny-invariant'
 import { SpotifyStrategy } from 'remix-auth-spotify'
 import { sessionStorage } from '@/modules/sessions/session.server'
+import { parseFullName } from 'parse-full-name'
+
+import { createOrUpdateSocialConnection, createUserFromOAuth } from '@/modules/users/oauth.server'
+import { findUserByEmail, generateUsernameFromEmail } from '@/modules/users/user.server'
 import { appUrl } from '@/utils/http'
 
 // Validate envars value.
 invariant(process.env.SPOTIFY_CLIENT_ID, 'SPOTIFY_CLIENT_ID must be set')
 invariant(process.env.SPOTIFY_CLIENT_SECRET, 'SPOTIFY_CLIENT_SECRET must be set')
 
-// -------------------------------------------------------------------------------------------------
-// Create a strategy for the Spotify authentication
-// See https://developer.spotify.com/documentation/general/guides/authorization/scopes
-// -------------------------------------------------------------------------------------------------
+/**
+ * Create a strategy for the Spotify authentication
+ * See https://developer.spotify.com/documentation/general/guides/authorization/scopes
+ */
 
 const scopes = ['user-read-email'].join(' ')
 
@@ -22,16 +26,49 @@ export const spotifyStrategy = new SpotifyStrategy(
     scope: scopes,
     sessionStorage,
   },
-  async ({ accessToken, refreshToken, extraParams, profile }) => ({
-    accessToken,
-    refreshToken,
-    expiresAt: Date.now() + extraParams.expiresIn * 1000,
-    tokenType: extraParams.tokenType,
-    user: {
-      id: profile.id,
-      email: profile.emails[0].value,
-      name: profile.displayName,
-      image: profile.__json.images?.[0].url,
-    },
-  }),
+  async ({ accessToken, refreshToken, extraParams, profile }) => {
+    const user = await findUserByEmail(profile.emails[0].value)
+
+    const socialAccount = {
+      accountType: 'oauth',
+      refreshToken: refreshToken || null,
+      accessToken: accessToken,
+      providerAccountId: profile.id,
+      provider: profile.provider,
+    }
+
+    const result = {
+      accessToken,
+      refreshToken,
+      expiresAt: Date.now() + extraParams.expiresIn * 1000,
+      tokenType: extraParams.tokenType,
+    }
+
+    // If user exists but doesn't have a google account, create one
+    if (user) {
+      await createOrUpdateSocialConnection(profile.emails[0].value, profile.provider, socialAccount)
+    }
+
+    if (!user) {
+      const generatedUsername = await generateUsernameFromEmail(profile.emails[0].value)
+      const username = profile.id || generatedUsername
+      const fullName = parseFullName(profile.displayName)
+
+      const newUser = await createUserFromOAuth({
+        userData: {
+          email: profile.emails[0].value,
+          firstName: fullName.first,
+          lastName: fullName.last,
+          imageUrl: profile.__json.images?.[0].url,
+          username,
+        },
+        ...socialAccount,
+      })
+
+      return { ...result, user: newUser }
+    }
+
+    // Taken from original sample.
+    return { ...result, user }
+  },
 )
