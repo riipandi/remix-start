6bbca36245d90a69ec33948dda1ec6bdf4ddb6a7
Aris Ripandi <riipandi@gmail.com>
2024-09-05 14:57:53 +0700

    chore: update healthcheck route

--- app/routes/healthz.tsx
+++ app/routes/healthz.tsx
@@ -2,9 +2,9 @@ import type { LoaderFunctionArgs } from '@remix-run/node'
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const host = request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
+  const url = new URL('/', `http://${host}`)
 
   try {
-    const url = new URL('/', `http://${host}`)
     // if we can connect to the database and make a simple query
     // and make a HEAD request to ourselves, then we're good.
     await Promise.all([
@@ -13,9 +13,9 @@ export async function loader({ request }: LoaderFunctionArgs) {
         if (!r.ok) return Promise.reject(r)
       }),
     ])
-    return new Response('OK')
+    return new Response('🫡 All is well!')
   } catch (error: unknown) {
     console.error('healthcheck ❌', { error })
-    return new Response('ERROR', { status: 500 })
+    return new Response('🔥 Unhealthy', { status: 500 })
   }
 }
